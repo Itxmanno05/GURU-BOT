cd66e07f32c47aeaa67d1b53531e3b619b127da8
Itx_manno_05 <149866936+Itxmanno05@users.noreply.github.com>
2024-02-17 21:30:29 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ import axios from 'axios'
 
 
 global.owner = [
-  [923140053156', '𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹', true]
+  ['923140053156', '𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹', true]
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = [923140053156'] 
-global.prems = [923140053156']
-global.allowed = [923140053156']
+global.mods = ['923140053156'] 
+global.prems = ['923140053156']
+global.allowed = ['923140053156']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
